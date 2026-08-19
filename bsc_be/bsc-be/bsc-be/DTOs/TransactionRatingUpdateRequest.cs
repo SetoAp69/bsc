5568e4d7ff42c0a3b4604617d9a6d12025b6d4aa
Adicipta <Adicipta@ACETRAINING-12.ad-ins.com>
2026-08-19 18:00:11 +0700

    add rating

---
+++ bsc_be/bsc-be/bsc-be/DTOs/TransactionRatingUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace bsc_be.DTOs
+{
+    public class TransactionRatingUpdateRequest
+    {
+        public long RatingId { get; set; }
+        public decimal StarRating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
---
+++ bsc_be/bsc-be/bsc-be/Services/RatingService.cs
@@ -0,0 +1,33 @@
+﻿using bsc_be.DTOs;
+using bsc_be.Models;
+using bsc_be.Repositories;
+
+namespace bsc_be.Services
+{
+    public class RatingService: IRatingService
+    {
+        private readonly IRepository<Rating> _ratingRepository;
+        public RatingService(IRepository<Rating> ratingRepository)
+        {
+            _ratingRepository = ratingRepository;
+        }
+
+        public async Task<RatingResponse?> UpdateRatingAsync(TransactionRatingUpdateRequest request)
+        {
+            var rating = await _ratingRepository.GetByIdAsync(request.RatingId);
+            if (rating == null)
+            {
+                return null;
+            }
+            rating.Star = request.StarRating;
+            rating.Comment = request.Comment;
+            await _ratingRepository.SaveChangesAsync();
+            return new RatingResponse
+            {
+                Id = rating.Id,
+                Rating = rating.Star,
+                Comment = rating.Comment
+            };
+        }
+    }
+}
